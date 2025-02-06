6727e181accd6b7c3e07ef34b0d7cc930af1c9a8
Valeriya Stange <valeriya.stange@gmail.com>
2025-02-06 10:29:11 +0100

    API laden

---
+++ script.js
@@ -0,0 +1,5 @@
+const gameOTApiUrl = "https://thronesapi.com/api/v2/Characters";
+const gameOTApiLimit = 10;
+const gameOTOffset = 0;
+let gameOTDataList = [];
+const gameOTList = document.getElementById("conteinerForEverything");
