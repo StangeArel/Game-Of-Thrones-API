0ef90e537efb4c4ecd42d5f504edf674f2ba76db
Valeriya Stange <valeriya.stange@gmail.com>
2025-02-06 10:30:22 +0100

    async function fetchGOTCharactersList

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const gameOTApiLimit = 10;
 const gameOTOffset = 0;
 let gameOTDataList = [];
 const gameOTList = document.getElementById("conteinerForEverything");
+
+async function fetchGOTCharactersList() {
+    const response = await fetch(gameOTApiUrl);
+    if (!response.ok) {
+        return console.error("API error");
+    }
+    const responseJson = await response.json();
+    const data = responseJson.slice(gameOTOffset, gameOTOffset + gameOTApiLimit);
+    gameOTDataList = [];
+    await fetchGOTCharactersDetails(data);
+}
